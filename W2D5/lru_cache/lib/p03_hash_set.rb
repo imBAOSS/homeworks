45f3eb25db07d051c6501d54ee336a484f49cf7c
imbaoss <baole6989@gmail.com>
2017-02-04 18:35:01 -0800

    Add lru_cache files

---
+++ W2D5/lru_cache/lib/p03_hash_set.rb
@@ -0,0 +1,48 @@
+require_relative 'p02_hashing'
+
+class HashSet
+  attr_reader :count
+
+  def initialize(num_buckets = 8)
+    @store = Array.new(num_buckets) { Array.new }
+    @count = 0
+  end
+
+  def insert(key)
+    resize! if num_buckets == @count
+    unless include?(key)
+      self[key] << key
+      @count += 1
+    end
+  end
+
+  def include?(key)
+    self[key].include?(key)
+  end
+
+  def remove(key)
+    self[key].delete(key)
+  end
+
+  private
+
+  def [](num)
+    # optional but useful; return the bucket corresponding to `num`
+    @store[num.hash % num_buckets]
+  end
+
+  def num_buckets
+    @store.length
+  end
+
+  def resize!
+    store = Array.new(num_buckets * 2) { Array.new }
+    @store.each do |bucket|
+      bucket.each do |item|
+        store[item.hash % num_buckets] << item
+      end
+    end
+
+    @store = store
+  end
+end
