99d7dfc3080ebbd7c1f95bcaece7edf0bc12d553
imBAOSS <baole6989@gmail.com>
2017-02-10 17:02:56 -0800

    Start 03_associateble.rb

--- W3D5/active-record-lite/lib/03_associatable.rb
+++ W3D5/active-record-lite/lib/03_associatable.rb
@@ -10,30 +10,52 @@ class AssocOptions
   )
 
   def model_class
-    # ...
+    @class_name.constantize
   end
 
   def table_name
-    # ...
+    "#{@class_name.downcase}s"
   end
 end
 
 class BelongsToOptions < AssocOptions
   def initialize(name, options = {})
-    # ...
+    defaults = {
+    class_name: "#{name.capitalize}",
+    primary_key: :id,
+    foreign_key: "#{name}_id".to_sym
+    }
+
+    defaults.keys.each do |key|
+      self.send("#{key}=", options[key] || defaults[key])
+    end
   end
 end
 
 class HasManyOptions < AssocOptions
   def initialize(name, self_class_name, options = {})
-    # ...
+    defaults = {
+      class_name: "#{name.capitalize.singularize}",
+      primary_key: :id,
+      foreign_key: "#{self_class_name.downcase}_id".to_sym
+    }
+
+    defaults.keys.each do |key|
+      self.send("#{key}=", options[key] || defaults[key])
+    end
   end
 end
 
 module Associatable
   # Phase IIIb
   def belongs_to(name, options = {})
-    # ...
+    options = BelongsToOptions(name, options)
+    define_method(name) do
+      val = self.send("#{options.foreign_key}")
+      mod_class = options.model_class
+      
+
+    end
   end
 
   def has_many(name, options = {})
@@ -47,4 +69,5 @@ end
 
 class SQLObject
   # Mixin Associatable here...
+  extend Associatable
 end
