9a4c83bf1ec3a3a1fc2075674de524c95a0d5c30
imBAOSS <baole6989@gmail.com>
2017-02-22 08:46:00 -0800

    Continue js hw

--- W5D3/intro_js_problems.js
+++ W5D3/intro_js_problems.js
@@ -1,7 +1,29 @@
 function madLib(str1, str2, str3) {
-  var str1Upper = str1.toUpperCase;
-  var str2Upper = str2.toUpperCase;
-  var str3Upper = str3.toUpperCase;
+  var str1Upper = str1.toUpperCase();
+  var str2Upper = str2.toUpperCase();
+  var str3Upper = str3.toUpperCase();
 
   console.log(`We shall ${str1Upper} the ${str2Upper} ${str3Upper}.`);
+}
+
+function isSubstring(searchString, subString) {
+  if (searchString.includes(subString)) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function fizzBuzz(array) {
+  var fbArray = [];
+
+  for (let i = 0; i < array.length; i++){
+    if ( array[i] % 3 && array[i] % 5) {
+      continue;
+    } else if ( array[i] % 3 || array[i] % 5 ) {
+      fbArray.push(array[i]);
+    }
+  }
+
+  return fbArray;
 }
