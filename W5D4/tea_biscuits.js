60bcffd830501055b135242067feee9e7b9ab0ed
imBAOSS <baole6989@gmail.com>
2017-02-23 09:37:18 -0800

    Fix tea_biscuits.js exercise

--- W5D4/tea_biscuits.js
+++ W5D4/tea_biscuits.js
@@ -5,32 +5,34 @@ const reader = readline.createInterface({
   output: process.stdout
 });
 
-// function teaAndBiscuits() {
-//   reader.question("Would you like some tea?", function (res1) {
-//     console.log(`You replied with ${res1}.`);
-//     reader.question("Would you like some biscuits?", function (res2) {
-//       console.log(`You replied with ${res2}.`);
+function teaAndBiscuits() {
+  reader.question("Would you like some tea?", function (res1) {
+    console.log(`You replied with ${res1}.`);
+    reader.question("Would you like some biscuits?", function (res2) {
+      console.log(`You replied with ${res2}.`);
+
+      const first = ( res1 === "yes" ? "do" : "don't");
+      const second = ( res2 === "yes" ? "do" : "don't");
+      console.log(`So that's a ${res1} for tea and a ${res2} for biscuits`);
+
+      reader.close();
+    });
+  });
+}
+
+// function teaAndBiscuits () {
+//   reader.question('Would you like some tea?', function (res) {
+//     console.log(`You replied ${res}.`);
+//     reader.question('Would you like some biscuits?', function (res2) {
+//       console.log(`You replied ${res2}.`);
 //
-//       const first = ( res1 === "yes" ? "do" : "don't");
-//       const second = ( res2 === "yes" ? "do" : "don't");
-//       console.log(`So that's a ${res1} for tea and a ${res2} for coffee`);
+//       const first = (res === 'yes') ? 'do' : 'don\'t';
+//       const second = (res2 === 'yes') ? 'do' : 'don\'t';
 //
+//       console.log(`So you ${first} want tea and you ${second} want biscuits.`);
 //       reader.close();
 //     });
 //   });
 // }
 
-function teaAndBiscuits () {
-  reader.question('Would you like some tea?', function (res) {
-    console.log(`You replied ${res}.`);
-    reader.question('Would you like some biscuits?', function (res2) {
-      console.log(`You replied ${res2}.`);
-
-      const first = (res === 'yes') ? 'do' : 'don\'t';
-      const second = (res2 === 'yes') ? 'do' : 'don\'t';
-
-      console.log(`So you ${first} want tea and you ${second} want biscuits.`);
-      reader.close();
-    });
-  });
-}
+teaAndBiscuits();
