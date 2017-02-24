3588be0c8b99a6f36c119573d2bfcbb4d51d3440
imBAOSS <baole6989@gmail.com>
2017-02-24 14:14:25 -0800

    Add asteroid files

--- W5D5/inheritance.js
+++ W5D5/inheritance.js
@@ -1,9 +1,10 @@
 Function.prototype.inherits = function (parentClass) {
-  function Surrogate() {}
-
-  Surrogate.prototype = parentClass.prototype;
-  this.prototype =  new Surrogate();
-  this.prototype.constructor = this;
+  // function Surrogate() {}
+  //
+  // Surrogate.prototype = parentClass.prototype;
+  // this.prototype =  new Surrogate();
+  // this.prototype.constructor = this;
+  this.prototype = Object.create(parentClass.prototype);
 };
 
 function Animal(name) {
@@ -14,22 +15,23 @@ Animal.prototype.eat = function () {
   console.log("I'm eating");
 };
 
-
 function Dog(name) {
-  this.name = name;
+  // this.name = name;
+  Animal.call(this, name);
 }
+Dog.inherits(Animal);
 
 Dog.prototype.woof = function () {
   console.log("Woof");
 };
 
 function Cat(name) {
-  this.name = name;
+  // this.name = name;
+  Animal.call(this, name);
 }
+
+Cat.inherits(Animal);
 
 Cat.prototype.meow = function () {
   console.log("Meow");
 };
-
-Dog.inherits(Animal);
-Cat.inherits(Animal);
