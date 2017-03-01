18b30c7ca036feb17d93a8f042454d6a9679d7aa
imBAOSS <baole6989@gmail.com>
2017-02-28 21:46:34 -0800

    Finish ajax hw

--- W6D3/first_ajax_skeleton/js/index.js
+++ W6D3/first_ajax_skeleton/js/index.js
@@ -1,5 +1,13 @@
 console.log("Hello from the JavaScript console!");
 
 // Your AJAX request here
-
+$.ajax({
+  type: "GET",
+  url: "http://api.openweathermap.org/data/2.5/weather?q=NY,NY&appid=bcb83c4b54aee8418983c2aff3073b3b",
+  success: function(data) {
+    console.log("Got weather!");
+    console.log(data);
+  }
+});
 // Add another console log here, outside your AJAX request
+console.log("I'm outside of the request");
